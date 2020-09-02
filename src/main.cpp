639aeb831624947569333427004ac37b97514e64
James G <jdgre1@gmail.com>
2020-09-02 23:39:22 +0200

    minor modifications to allow orientation correction, replaced raw- with smart pointer

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/utils/trace.hpp>
 #include <opencv2/text/ocr.hpp>
-
+#include <memory.h>
 #include <leptonica/allheaders.h>
 #include <tesseract/baseapi.h>
 
@@ -17,12 +17,14 @@ using namespace cv;
 using namespace std;
 using namespace cv::text;
 
+cv::Mat pix8ToMat(Pix* pix8);
+
 
 int main(int argc, char* argv[])
 {
-	//Ptr<OCRTesseract> ocr = OCRTesseract::create();
-	
-	tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
+	//tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI(); // No need to delete with unique pointer assigned 
+	auto api = std::make_unique<tesseract::TessBaseAPI>();
+
 	// Initialize tesseract-ocr with English, without specifying tessdata path
 	if (api->Init(NULL, "eng")) {
 		cerr << "Could not initialize tesseract.\n";
@@ -38,16 +40,20 @@ int main(int argc, char* argv[])
 	std::string file;
 	std::string folder_dir = "../../data/PDF_imgs/";
 	std::string imgs[6] = { folder_dir + "collections-0.jpg", folder_dir + "collections-1.jpg", folder_dir + "collections-2.jpg", folder_dir + "collections-3.jpg", folder_dir + "collections-4.jpg", folder_dir + "collections-5.jpg" };
-
+	float major_rotate_angle;
+	Mat img_mat;
+	Mat rot_mat;
+	PIX* image;
+	// 1. Correct orientation
 	for (int i = 0; i < sizeof(imgs) / sizeof(imgs[0]); i++) {
 
 		std::string file = imgs[i];
 		const char* inputfile = file.c_str();
-		PIX* image = pixRead(inputfile);
+		image = pixRead(inputfile);
 
 		api->SetPageSegMode(tesseract::PSM_AUTO_OSD);
 		api->SetImage(image);
-		api->Recognize(0);
+		//api->Recognize(0); // too time-consuming and not necessary to determine dpi
 		it = api->AnalyseLayout();
 		it->Orientation(&orientation, &direction, &order, &deskew_angle);
 
@@ -55,72 +61,115 @@ int main(int argc, char* argv[])
 			"\nWritingDirection: " << direction <<
 			"\nTextlineOrder: " << order <<
 			"\nDeskew angle: " << deskew_angle << "\n";
-	}
 
-	delete api;
-	
-	
-	for (int i = 0; i < sizeof(imgs)/ sizeof(imgs[0]); i++) {
-		
-		Mat image;
-		image = imread(imgs[i], IMREAD_COLOR);
-		int winH = int(image.rows * 0.05);
-		int winL = int(image.cols * 0.3);
-		int startY = 0;// int(image.rows * 0.67);
-		int stepSize = 50;
-		int disp = int(winH / 2);
-		bool lastRow = false;
-		for (size_t j = startY; j < image.rows; j += disp) {
-			if (image.rows < (j + winH)) {
-				j = image.rows - winH;
-				lastRow = true;
-			}
+		// Use OpenCV to read-image and correct rotation:
+		major_rotate_angle = 0;
+		img_mat = imread(imgs[i], IMREAD_COLOR);
+		Point2f src_center(img_mat.cols / 2.0F, img_mat.rows / 2.0F);
 
-			cout << "j = " << j << endl;
-			bool lastCol = false;
-
-			for (size_t k = 0; k <= (image.cols - winL); k += stepSize) {
-				cout << "k = " << k << endl;
-				if (image.cols < (k + winL)) {
-					k = image.cols - winL;
-					lastCol = true;
-				}
-				cv::Rect roi = Rect(k, j, winL, winH);
-				imshow("Image", image(roi));
-				waitKey(100);
-
-				if (lastCol)
-					break;
-			}
-
-			if (lastRow)
-				break;
+		if (orientation == 1) {
+			major_rotate_angle = -90;
+		}
+		else if (orientation == 2) {
+			major_rotate_angle = -180;
 		}
 
-		//imshow("Image", image);
-		//waitKey();
+		else if (orientation == 3) {
+			major_rotate_angle = 90;
+		}
+
+		Mat rot_mat = getRotationMatrix2D(src_center, deskew_angle - major_rotate_angle, 1);
+		cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), img_mat.size(), deskew_angle - major_rotate_angle).boundingRect2f(); //https://stackoverflow.com/questions/22041699/rotate-an-image-without-cropping-in-opencv-in-c
+		// adjust transformation matrix
+		rot_mat.at<double>(0, 2) += bbox.width / 2.0 - img_mat.cols / 2.0;
+		rot_mat.at<double>(1, 2) += bbox.height / 2.0 - img_mat.rows / 2.0;
+
+		cv::Mat dst;
+		warpAffine(img_mat, dst, rot_mat, bbox.size());
+		Size size(int(dst.cols/2), int(dst.rows / 2));//the dst image size,e.g.100x100
+		resize(dst, dst, size);//resize image
+
+		imshow("img_mat", img_mat);
+		imshow("dst", dst);
+		waitKey();
+
 	}
-	delete api;
+
 	return 0;
 }
 
-
-/* def sliding_window(image, stepSize, windowSize):
-	# slide a window across the image
-    startY = 0 #int(image.shape[0] * 0.67) #0
-    endY = int(image.shape[0]) # int(image.shape[0]*0.46)
-
-    for y in range(startY, endY, stepSize):
-        for x in range(0, image.shape[1], stepSize):
-            # yield the current window
-            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+/*
+// delete api; (unique pointer used)
 
 
-for (x, y, window) in sliding_window(img, stepSize=50, windowSize=(winW, winH)):
+for (int i = 0; i < sizeof(imgs)/ sizeof(imgs[0]); i++) {
 
-            # if the window does not meet our desired window size, ignore it
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
+	Mat image;
+	image = imread(imgs[i], IMREAD_COLOR);
+	int winH = int(image.rows * 0.05);
+	int winL = int(image.cols * 0.3);
+	int startY = 0;// int(image.rows * 0.67);
+	int stepSize = 50;
+	int disp = int(winH / 2);
+	bool lastRow = false;
+	for (size_t j = startY; j < image.rows; j += disp) {
+		if (image.rows < (j + winH)) {
+			j = image.rows - winH;
+			lastRow = true;
+		}
+
+		cout << "j = " << j << endl;
+		bool lastCol = false;
+
+		for (size_t k = 0; k <= (image.cols - winL); k += stepSize) {
+			cout << "k = " << k << endl;
+			if (image.cols < (k + winL)) {
+				k = image.cols - winL;
+				lastCol = true;
+			}
+			cv::Rect roi = Rect(k, j, winL, winH);
+			imshow("Image", image(roi));
+			waitKey(100);
+
+			if (lastCol)
+				break;
+		}
+
+		if (lastRow)
+			break;
+	}
+
+	//imshow("Image", image);
+	//waitKey();
+}
+//delete api;
+return 0;
+}
 */
+
+
+Pix* mat8ToPix(cv::Mat* mat8)
+{
+	Pix* pixd = pixCreate(mat8->size().width, mat8->size().height, 8);
+	for (int y = 0; y < mat8->rows; y++) {
+		for (int x = 0; x < mat8->cols; x++) {
+			pixSetPixel(pixd, x, y, (l_uint32)mat8->at<uchar>(y, x));
+		}
+	}
+	return pixd;
+}
+
+cv::Mat pix8ToMat(Pix* pix8)
+{
+	cv::Mat mat(cv::Size(pix8->w, pix8->h), CV_8UC1);
+	uint32_t* line = pix8->data;
+	for (uint32_t y = 0; y < pix8->h; ++y) {
+		for (uint32_t x = 0; x < pix8->w; ++x) {
+			mat.at<uchar>(y, x) = GET_DATA_BYTE(line, x);
+		}
+		line += pix8->wpl;
+	}
+	return mat;
+}
 
 
